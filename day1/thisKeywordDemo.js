854eeb4a89c77ba619e248859c9a63b2fda7dfb7
pradeep <pradeep.kumar44@gmail.com>
2019-03-18 10:33:30 +0530

    committing the changes

---
+++ day1/thisKeywordDemo.js
@@ -0,0 +1,6 @@
+//this keyword exmplaination
+/*
+  1. Inside an object
+  2. inside a function
+  3. Using new operator
+*/
--- day1/varDemo.js
+++ day1/varDemo.js
@@ -1,2 +1,36 @@
 
-  var 
+for ( let i = 0; i < 10; i++){
+    console.log('The value of i is '+i);
+}
+
+//console.log('The value of i outside the block is '+i)
+
+
+var demo = function(){
+    var i;
+    var m = 15;
+    console.log('The value of i is '+i);
+    console.log('The value of j is '+j);
+    console.log('The value of k i '+k);
+    console.log('The value of m is '+m);
+     i = 10;
+     var j;
+}
+
+//demo();
+
+
+/*var undesiredFunction = function(){
+    var flag = true;
+    if(flag){
+        var message = "The value is set inside the block";
+    }
+    console.log(message);
+}
+
+undesiredFunction();*/
+
+let a = 20;
+a = 45;
+console.log('The value of a is '+a);
+
