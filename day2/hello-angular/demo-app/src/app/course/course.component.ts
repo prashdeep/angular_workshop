c1020f2ebf0bfdca678fd80a34741149009500ad
pradeep <pradeep.kumar44@gmail.com>
2019-03-19 16:23:07 +0530

    committing the component

--- day2/hello-angular/demo-app/src/app/course/course.component.ts
+++ day2/hello-angular/demo-app/src/app/course/course.component.ts
@@ -7,11 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseComponent {
 
-    course={
+    courses=[
+      {
        name:"React",
        duration: 10,
        price:15000,
        instructor: "Kishore"
-    }
+      },
+      {
+        name:"Java 8",
+        duration: 8,
+        price:10000,
+        instructor: "Vinod"
+       },
+       {
+        name:"Angular",
+        duration: 10,
+        price:18000,
+        instructor: "Vikas"
+       },
+       {
+        name:"Spring Boot",
+        duration: 10,
+        price:25000,
+        instructor: "Vishnu"
+       },
+       {
+        name:"VueJs",
+        duration: 10,
+        price:15000,
+        instructor: "Aman"
+       },
+       {
+        name:"Webpack",
+        duration: 2,
+        price:5000,
+        instructor: "Aman"
+       } 
+  ]
+
+  details(course){
+    console.log(`came inside the details method.. ${course.name}`)
+  }
   
 }
