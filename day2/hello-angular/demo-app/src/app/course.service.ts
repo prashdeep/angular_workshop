f00f8a9ba9445d804d58310392919aa5b2b35f1a
pradeep <pradeep.kumar44@gmail.com>
2019-03-20 14:16:59 +0530

    committing the service code

--- day2/hello-angular/demo-app/src/app/course.service.ts
+++ day2/hello-angular/demo-app/src/app/course.service.ts
@@ -7,73 +7,84 @@ import { Course } from './Course';
 export class CourseService {
 
   constructor() { }
+  courses:Course[] = [
+    {
+     name:"React",
+     duration: 10,
+     price:15000,
+     rating:5.677,
+     startDate: new Date(2019,10,10),
+     offer:true,
+     instructor: "Kishore",
+     imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+    },
+    {
+      name:"Java 8",
+      duration: 8,
+      price:10000,
+      rating:3.4566,
+      startDate: new Date(2019,12,10),
+      offer:false,
+      instructor: "Vinod",
+      imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+     },
+     {
+      name:"Angular",
+      duration: 10,
+      price:18000,
+      rating:6.675,
+      startDate: new Date(2019,3,3),
+      offer:false,
+      instructor: "Vikas",
+      imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+
+     },
+     {
+      name:"Spring Boot",
+      duration: 10,
+      price:25000,
+      rating:4.4534,
+      startDate: new Date(2019,8,10),
+      offer:true,
+      instructor: "Vishnu",
+      imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+
+     },
+     {
+      name:"VueJs",
+      duration: 10,
+      price:15000,
+      rating:5.444,
+      startDate: new Date(2019,8,8),
+      offer:true,
+      instructor: "Aman",
+      imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+
+     },
+     {
+      name:"Webpack",
+      duration: 2,
+      price:5000,
+      rating:4.343,
+      startDate: new Date(2019,5,5),
+      offer:false,
+      instructor: "Aman",
+      imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+
+     } 
+];
 
   fetchAllCourses():Course[]{
-    return [
-      {
-       name:"React",
-       duration: 10,
-       price:15000,
-       rating:5.677,
-       startDate: new Date(2019,10,10),
-       offer:true,
-       instructor: "Kishore",
-       imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-      },
-      {
-        name:"Java 8",
-        duration: 8,
-        price:10000,
-        rating:3.4566,
-        startDate: new Date(2019,12,10),
-        offer:false,
-        instructor: "Vinod",
-        imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-       },
-       {
-        name:"Angular",
-        duration: 10,
-        price:18000,
-        rating:6.675,
-        startDate: new Date(2019,3,3),
-        offer:false,
-        instructor: "Vikas",
-        imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+   return this.courses
+  }
 
-       },
-       {
-        name:"Spring Boot",
-        duration: 10,
-        price:25000,
-        rating:4.4534,
-        startDate: new Date(2019,8,10),
-        offer:true,
-        instructor: "Vishnu",
-        imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+  addCourse(course:Course){
+    this.courses.push(course);
+  }
 
-       },
-       {
-        name:"VueJs",
-        duration: 10,
-        price:15000,
-        rating:5.444,
-        startDate: new Date(2019,8,8),
-        offer:true,
-        instructor: "Aman",
-        imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-
-       },
-       {
-        name:"Webpack",
-        duration: 2,
-        price:5000,
-        rating:4.343,
-        startDate: new Date(2019,5,5),
-        offer:false,
-        instructor: "Aman",
-        imgSrc:"https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-
-       } 
-  ];
+  deleteCourse(courseName){
+    console.log(courseName);  
+    let index = this.courses.findIndex((course)=> course.name === courseName);
+    this.courses.splice(index, 1);
   }
 }
--- day2/hello-angular/demo-app/src/app/course/course.component.ts
+++ day2/hello-angular/demo-app/src/app/course/course.component.ts
@@ -12,16 +12,14 @@ export class CourseComponent implements OnInit {
 
   availOffer:boolean = false;
   courses:Course[] = [];
+  currencyCode = Config.currency;
 
   //Angular will add the dependency during creation of the component.
   constructor(private courseService:CourseService){
   }
-
   ngOnInit(): void {
     this.courses = this.courseService.fetchAllCourses();
   }
-
-
   details(course){
     console.log(`came inside the details method.. ${course.name}`)
   }
@@ -33,24 +31,15 @@ export class CourseComponent implements OnInit {
     course.offer = false;
     course.instructor  ="John";
     course.name = courseName.value;
-
-    this.courses.push(course);
-
+    this.courseService.addCourse(course)
   }
 
   deleteCourse(courseName){
-    console.log(courseName);  
-    let index = this.courses.findIndex((course)=> course.name === courseName);
-    this.courses.splice(index, 1);
+   this.courseService.deleteCourse(courseName);
   }
 
   handleOffer(){
     //console.log(`The checkbox status is ${status}`)
     this.courses.forEach(course => course.offer = this.availOffer);
   }
-
-  currencyCode = Config.currency;
-
-
-  
 }
