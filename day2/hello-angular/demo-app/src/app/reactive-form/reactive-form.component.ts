1e175468b2452679dd0b151e35230bed619c9f24
pradeep <pradeep.kumar44@gmail.com>
2019-03-21 13:34:42 +0530

    committing to remove transpiling error

--- day2/hello-angular/demo-app/src/app/reactive-form/reactive-form.component.ts
+++ day2/hello-angular/demo-app/src/app/reactive-form/reactive-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { CustomValidators } from '../CustomValidators';
+//import { CustomValidators } from '../CustomValidators';
 
 @Component({
   selector: 'app-rform',
